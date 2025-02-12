410c2ff355798996c1855e90bcf88f858ab48873
To Ngoc Hung <103498379+Kas1902@users.noreply.github.com>
2025-02-12 18:57:06 +0700

    init Gallery and config for svg import

---
+++ Gallery/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: false,
+  extends: '@react-native',
+  'prettier/prettier': [
+    'error',
+    {
+      endOfLine: 'auto',
+    },
+  ],
+};
---
+++ Gallery/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Text, StatusBar, View} from 'react-native';
+import Button from './components/Button/Button';
+import InputField from './components/InputField/InputField';
+import Explore from './assets/explore.svg';
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar barStyle={'dark-content'} backgroundColor={'#79D7BE'} />
+      <Text>Hello, world!</Text>
+      <Button
+        content={'Press me!'}
+        onPress={() => console.log('Button pressed!')}
+        width="80%"
+      />
+      <InputField
+        title={'Name'}
+        placeholder={'Enter your name'}
+        secure={true}
+        keyboardType={'password'}
+      />
+      <Explore width={100} height={100} />
+    </View>
+  );
+};
+
+export default App;
